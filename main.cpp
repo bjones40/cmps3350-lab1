60d0f5addfc99d23c05c2d3d1407c493645c5e8b
Benjamin Jones <bjones@odin.cs.csubak.edu>
2021-02-02 10:29:26 -0800

    lab1 changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 // CMPS 3350 - lab 1
 // Prints each command-line argument passed into the program, one per line
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char ** argv)
 {
+    if(argc > 1)
+    {
     for(int i = 1; i < argc; i++) {
         printf("arg %d: %s\n", i, argv[i]);
+    }
+    }
+    else
+    {
+        perror("Usage: ./filename <arguments>\n");
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
